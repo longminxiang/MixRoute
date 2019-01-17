d1f11f3eed01a5edfbf32d88faaba30fb03ed95a
eric <longminxiang@163.com>
2019-01-17 17:47:03 +0800

    add barbugfix

---
+++ Classes/UITabBar+BugFix.h
@@ -0,0 +1,17 @@
+//
+//  UITabBar+BugFix.h
+//  MixRoute
+//
+//  Created by Eric Lung on 2018/11/30.
+//  Copyright © 2018年 YOOEE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MixBugFixTabBar : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/UITabBar+BugFix.m
@@ -0,0 +1,38 @@
+//
+//  UITabBar+BugFix.m
+//  MixRoute
+//
+//  Created by Eric Lung on 2018/11/30.
+//  Copyright © 2018年 YOOEE. All rights reserved.
+//
+
+#import "UITabBar+BugFix.h"
+#import <objc/runtime.h>
+
+@implementation MixBugFixTabBar
+
++ (void)load
+{
+    if (@available(iOS 12.1, *)) {
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            id (^block)(Class, SEL, IMP)  = ^id(Class originClass, SEL originSEL, IMP originIMP) {
+                return ^(UIView *view, CGRect rect) {
+                    if ([view isKindOfClass:originClass]) {
+                        if (!CGRectIsEmpty(view.frame) && CGRectIsEmpty(rect))  return;
+                    }
+                    void (*originSelectorIMP)(id, SEL, CGRect) = (void (*)(id, SEL, CGRect))originIMP;
+                    originSelectorIMP(view, originSEL, rect);
+                };
+            };
+
+            Class class = NSClassFromString(@"UITabBarButton");
+            SEL sel = @selector(setFrame:);
+            Method originMethod = class_getInstanceMethod(class, sel);
+            id impBlock = block(class, sel, method_getImplementation(originMethod));
+            method_setImplementation(originMethod, imp_implementationWithBlock(impBlock));
+        });
+    }
+}
+
+@end
